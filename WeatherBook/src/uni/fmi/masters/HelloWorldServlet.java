478643117c43f1633dcf823e2fb35263f4edf143
AlexanderPPetrov <apetrovdev@gmail.com>
2019-10-20 16:23:13 +0300

    Lesson 1 езици и среди за програмиране

---
+++ WeatherBook/src/uni/fmi/masters/HelloWorldServlet.java
@@ -0,0 +1,58 @@
+package uni.fmi.masters;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorldServlet
+ */
+@WebServlet("/HelloWorldServlet")
+public class HelloWorldServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorldServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		if(username.equalsIgnoreCase("gotin") && password.equals("gotin321")) {
+			//go to home
+			response.sendRedirect("home.html");
+		}else {
+			//gotoerror
+			RequestDispatcher rd = request.getRequestDispatcher("error.html");
+			
+			rd.forward(request, response);
+		}
+		
+		
+		response.getWriter().append("Hello " + username + " to this "
+				+ "Cruel World where NOTHING is safe! Your password is:"
+				+ password);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
