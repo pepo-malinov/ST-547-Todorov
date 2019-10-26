3fe02415aff1f07a6185696c203facfa5f4d9fe5
AlexanderPPetrov <apetrovdev@gmail.com>
2019-10-26 16:58:19 +0300

    lesson on 26.10.2019

--- WeatherBook/src/uni/fmi/masters/HelloWorldServlet.java
+++ WeatherBook/src/uni/fmi/masters/HelloWorldServlet.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import uni.fmi.masters.beans.UserBean;
+
 /**
  * Servlet implementation class HelloWorldServlet
  */
@@ -28,23 +30,65 @@ public class HelloWorldServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		String router = request.getParameter("router");
+		
+		switch(router) {
+			case "login":
+				goToLogin(request, response);
+			break;
+			case "register":
+				registerUser(request, response);
+				break;
+		}
+		
+		
+
+	}
+
+	private void registerUser(HttpServletRequest request, HttpServletResponse response) {
+		
+		String username = request.getParameter("username");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		String repeatPassword = request.getParameter("repeatPassword");
+		
+		if(password.equals(repeatPassword)) {
+			UserBean user = new UserBean(username, email, password);
+			request.setAttribute("user", user);
+			
+			redirect("profile.jsp", request, response);
+			
+		}else {
+			request.setAttribute("message", "Password missmatch!");
+			redirect("error.jsp", request, response);			
+		}		
+		
+	}
+	
+	private void redirect(String page, HttpServletRequest request, HttpServletResponse response) {
+		
+		RequestDispatcher rd = request.getRequestDispatcher(page);
+		
+		try {
+			rd.forward(request, response);
+		} catch (ServletException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	private void goToLogin(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
 		
 		if(username.equalsIgnoreCase("gotin") && password.equals("gotin321")) {
-			//go to home
-			response.sendRedirect("home.html");
-		}else {
-			//gotoerror
-			RequestDispatcher rd = request.getRequestDispatcher("error.html");
-			
-			rd.forward(request, response);
+			redirect("home.html", request, response);
+		}else {			
+			request.setAttribute("message", "Wrong password information!");
+			redirect("error.jsp", request, response);
 		}
-		
-		
-		response.getWriter().append("Hello " + username + " to this "
-				+ "Cruel World where NOTHING is safe! Your password is:"
-				+ password);
 	}
 
 	/**
